63f95392260f1dcbee0d7f93b3df5f3749b4ffe4
Yashwant  Mandade <126948100+Itsyassss21@users.noreply.github.com>
2024-08-11 00:56:40 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,18 @@
+import qrcode
+
+input_URL = "https://images.hdqwalls.com/download/2022-the-batman-movie-8k-kb-1920x1080.jpg"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=15,
+    border=4,
+)
+
+qr.add_data(input_URL)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="red", back_color="white")
+img.save("url_qrcode.png")
+
+print(qr.data_list)
